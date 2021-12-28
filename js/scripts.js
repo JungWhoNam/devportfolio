0d0e5f9d158c562a843be41762ee21fa0a33c7f0
namxx054 <namxx054@umn.edu>
2021-12-28 11:11:44 +0900

    Support js modules

--- js/scripts.js
+++ js/scripts.js
@@ -63,21 +63,20 @@ for (let i = 0; i < accordions.length; i++) {
     // Create timeline
     $('#experience-timeline').each(function() {
 
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
+        var userContent = $(this).children('div'); // user content
 
         // Create each timeline block
-        $userContent.each(function() {
+        userContent.each(function() {
             $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
         });
 
         // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
+        $(this).find('.vtimeline-point').each(function() {
             $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
         });
 
         // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
+        $(this).find('.vtimeline-content').each(function() {
             var date = $(this).data('date');
             if (date) { // Prepend if exists
                 $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
